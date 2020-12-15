587c918afc89ab848a3825404c2400c66137f096
sandeepmj <sjnews@gmail.com>
2020-12-15 16:52:30 -0500

    removed slash

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pipping',
     version='0.0.1',
     description='My private package from private github repo',
-    url='git@github.com:/sandeepmj/pipping.git',
+    url='git@github.com:sandeepmj/pipping.git',
     author='Sandeep Junnarkar',
     author_email='sandeep@journalism.cuny.edu',
     license='unlicense',
