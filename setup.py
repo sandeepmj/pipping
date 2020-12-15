764b747e4efe723ee2548f5c05807fd143a66ff4
sandeepmj <sjnews@gmail.com>
2020-12-15 16:42:52 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='myfunc',
+    name='pipping',
     version='0.0.1',
     description='My private package from private github repo',
     url='git@github.com:/sandeepmj/pipping.git',
