e2822fb6bd656e9d19d23a2dddbbda4836adfb34
sandeepmj <sjnews@gmail.com>
2020-12-15 16:33:54 -0500

    reorg

---
+++ myfunc/__init__.py
@@ -0,0 +1,9 @@
+## function to export a dictionary to csv
+# def export2csv(a_list, filename):
+#     '''
+#     provide list of dicts name first
+#     provide filename as a string
+#     '''
+#     df = pd.DataFrame(a_list)
+#     df.to_csv(filename, encoding='utf-8', index=False)
+#     print(f"{filename} is in your project folder!")
